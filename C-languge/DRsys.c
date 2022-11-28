fd607460e49de0f92031829757c7cffb47e62b43
Youngjo Kim <youngjo2305@gmail.com>
2022-11-29 02:11:43 +0900

    Last_commit

--- C-languge/DRsys.c
+++ C-languge/DRsys.c
@@ -1,9 +1,10 @@
 #define _CRT_SECURE_NO_WARNINGS //보안 문제로 인한 오류를 방지하기 위함
 #include <stdio.h>
-#include <stdlib.h> // rand 함수 쓰기위해 넣음
-#include <time.h>  // srand값을 랜덤으로 바꾸기 위해 넣음
-#include <windows.h>
-#include <string.h>
+#include <stdlib.h> // rand 함수 쓰기위함
+#include <time.h>  // srand값을 랜덤으로 바꾸기 위함
+#include <windows.h> //cls와 Sleep사용하기 위함
+#include <string.h>  // 파일 함수 사용하기 위함
+#define Five 5
 
 typedef struct _Student
 {
@@ -16,13 +17,13 @@ typedef struct _Student
 }Student; //구조체에 별칭을 지정, 전역변수
 void loading(void);  //로딩창을 보여준다.
 int menu(void);      //메뉴창 출력 및 원하는 학생의 정보를 입력을 받는다.
-void Make_struct(Student* student, char file_name[20]);
+void Make_struct(Student* student, char file_name[20]); // 공백으로 구분된 텍스트 파일의 정보를 구조체로 만들어 주는 함수 
 void s_print(Student* student, char name[20], int q);  //원하는 메뉴에 맞는 답을 밑 s_로 시작하는 4개의 함수를 이용하여 출력해준다.
 void s_Class(Student* student, int a);    //학과 출력 
 void s_Num(Student* student, int a);       //학번 출력 
 void s_Age(Student* student, int a);       //나이 반환 
 void s_Grade(Student* student, int a);     //학점 반환
-void Active_Active(int* Text_name);
+void Active_Active(int* Text_name); // 구조체를 만드는데 들어가는 텍스트를 바꿔준다.  이중포인터 사용
 int main() {
 	char* Data = "Data.txt", oryu[] = { "(오류발생)" };;
 	char buffer[1001], * token;
@@ -34,7 +35,7 @@ int main() {
 	loading();                             //로딩한다.
 boot:
 	printf("현재 정보가 입력된 학생의 이름\n");   //입력된 학생들을 알려줌
-	for (i = 0; i < 5; i++)
+	for (i = 0; i < Five; i++)
 		printf("%s\n", student[i].name);
 	while (1) {                                    //r_int2를 통해 25%확률로 뻑이가게 만듬 r_int는 오류 발생시 구조체 값을 랜덤으로 바꿔버림
 		int changer, r_int = rand() % 99999, r_int2 = r_int % 4;
@@ -59,12 +60,12 @@ boot:
 				break;
 			}
 			else {
-				Active_Active(&Data);
+				Active_Active(&Data);             //메인 밖의 함수에서 Data2 파일로 student_save 구조체를 초기화
 				Make_struct(student_save, Data);
-				for (i = 0; i < 5; i++) {
+				for (i = 0; i < Five; i++) {          // student_save구조체를 이용하여 student 구조체를 초기화
 					student[i] = student_save[i];
 				}
-				goto boot;
+				goto boot; //(goto를 써보자는 목적으로 사용).
 			}
 		}
 
@@ -73,8 +74,8 @@ boot:
 			scanf("%s", name);
 			s_print(student, name, changer);   //원하는 정보를 출력해줌.
 		}
-		else if (changer == 5)
-			goto finish;  //프로그램 끝내기 break와 같은 의미 (goto를 쓰는 목적으로 goto 사용).
+		else if (changer == Five)
+			goto finish;  //프로그램 끝내기 break와 같은 의미 (goto를 써보자는 목적으로 사용).
 		else {  //이상한 값 적을시 프로그램을 끝내버림
 			printf("잘못된 범위의 값을 입력하셨습니다. 프로그램을 종료합니다.\n");
 			break;
@@ -83,7 +84,7 @@ boot:
 
 finish:        //프로그램 종료 구문
 	printf("사용해주셔서 감사합니다.\n");
-	printf("바뀐 Data.txt 파일 원본은 Data3.txt에 새로 만들어 놨습니다.");
+	printf("바뀐 Data.txt 파일 원본은 Data3.txt에 새로 만들어 놨습니다."); //Data.txt와 Data2.txt 파일까지 변형되었을 경우에 복사하면 되는 복사본 생성
 	FILE* Data_file;
 	Data_file = fopen("Data3.txt", "w");
 	fputs("Kim Computer_science 2022001 20 4.1\nLee art 2022002 21 3.8\nPark media 2022003 22 3.2\nChoi Physical_Education 2022004 24 4.0\nJeong Computer_science 2022005 60 4.5", Data_file);
@@ -141,7 +142,7 @@ int menu(void) {
 	return changer;
 }
 void s_print(Student* student, char name[20], int s_want) {
-	void (*s_infomation[4])(Student * student, int) = { s_Class, s_Num, s_Age, s_Grade };
+	void (*s_infomation[4])(Student * student, int) = { s_Class, s_Num, s_Age, s_Grade }; // 함수 포인터 배열
 	int count = 0;
 	for (int i = 0; i < 5; i++)
 	{
@@ -150,7 +151,7 @@ void s_print(Student* student, char name[20], int s_want) {
 		}
 		else
 			count += 1;
-		if (count == 5)
+		if (count == Five)
 			printf("\n찾으시는 학생이 존재하지 않습니다. 초기화면으로 돌아갑니다.\n\n");
 	}
 };
